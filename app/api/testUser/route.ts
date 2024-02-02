11b0c6570d4c93246ae1c79ffa76f3f1d0711a67
ThomasSurette <tsurette111@gmail.com>
2024-02-02 11:34:08 -0500

    deleted test route

--- app/api/testUser/route.ts
+++
@@ -1,7 +0,0 @@
-import { currentUser } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-
-export async function GET(){
-    const user = await currentUser();
-    return NextResponse.json(user, { status: 200 });
-}
