26e255ff79c4a439b6ddb0ed7d872de7108bc69a
ThomasSurette <tsurette111@gmail.com>
2024-02-09 23:01:31 -0500

    header style change

--- components/event-header.tsx
+++ components/event-header.tsx
@@ -28,8 +28,8 @@ export function EventHeader({ eventId }: { eventId: string }) {
       : null;
 
   return (
-    <header className="flex flex-col mb-5 max-w-2xl mx-auto gap-3">
-      <h1 className="text-4xl font-heading">{title}</h1>
+    <header className="flex flex-col my-5 max-w-2xl mx-auto gap-3">
+      <h1 className="text-5xl font-heading font-medium">{title}</h1>
       <div className="flex flex-col gap-2">
         <div className="flex items-center gap-1 text-muted-foreground">
           <Icons.location className="w-6 h-6" />
