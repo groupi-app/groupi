2357c235bda1ca1769607eee7a05b2b3fed0a765
ThomasSurette <tsurette111@gmail.com>
2025-03-04 19:03:37 -0500

    fixed default states

--- app/(event)/event/[eventId]/change-date/multi/page.tsx
+++ app/(event)/event/[eventId]/change-date/multi/page.tsx
@@ -1,12 +1,28 @@
 import { EditEventMultiDate } from "@/components/edit-event-multi-date";
 import { NewEventMultiDate } from "@/components/new-event-multi-date";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { eventId: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: { eventId: string };
+}) {
   const { eventId } = params;
+  const res = await db.event.findUnique({
+    where: { id: eventId },
+    select: { potentialDateTimes: true },
+  });
+  if (!res) {
+    notFound();
+  }
+  const dates = res.potentialDateTimes
+    ? res.potentialDateTimes.map((pdt) => pdt.dateTime)
+    : undefined;
   return (
     <div className="container max-w-4xl">
       <h1 className="text-4xl font-heading mt-10">Event Date/Time Options</h1>
-      <EditEventMultiDate eventId={eventId} />
+      <EditEventMultiDate eventId={eventId} dates={dates} />
     </div>
   );
 }
--- app/(event)/event/[eventId]/change-date/single/page.tsx
+++ app/(event)/event/[eventId]/change-date/single/page.tsx
@@ -1,11 +1,25 @@
 import { EditEventSingleDate } from "@/components/edit-event-single-date";
+import { db } from "@/lib/db";
+import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { eventId: string } }) {
+export default async function Page({
+  params,
+}: {
+  params: { eventId: string };
+}) {
   const { eventId } = params;
+  const res = await db.event.findUnique({
+    where: { id: eventId },
+    select: { chosenDateTime: true },
+  });
+  if (!res) {
+    notFound();
+  }
+  const datetime = res.chosenDateTime ?? undefined;
   return (
     <div className="container max-w-4xl">
       <h1 className="text-4xl font-heading mt-10">Event Date/Time</h1>
-      <EditEventSingleDate eventId={eventId} />
+      <EditEventSingleDate eventId={eventId} datetime={datetime} />
     </div>
   );
 }
