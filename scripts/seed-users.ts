b155eeb4229ee997a5de0c46c97c8b8b3851a8c6
TheiaSurette <tsurette111@gmail.com>
2025-05-17 14:17:43 -0400

    DX stuff

---
+++ scripts/seed-users.ts
@@ -0,0 +1,51 @@
+// prettier-ignore
+import 'tsconfig-paths/register';
+import { seedUsers as users } from '@/data/seed-users';
+import { db } from '@/lib/db';
+import { createClerkClient, User } from '@clerk/backend'; // Import createClerkClient
+
+export default async function seedUsers() {
+  try {
+    const clerkClient = createClerkClient({
+      // Use createClerkClient
+      secretKey: process.env.CLERK_SECRET_KEY,
+    });
+    const { data: existingUsers } = await clerkClient.users.getUserList();
+    await Promise.all(existingUsers.map((user: User) => clerkClient.users.deleteUser(user.id)));
+    await db.person.deleteMany();
+    const people = [];
+    for (const user of users) {
+      // Use for...of for sequential async operations
+      const userObj = await clerkClient.users.createUser({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.username,
+        emailAddress: [user.email],
+      });
+      const firstName = userObj.firstName || '';
+      const lastName = userObj.lastName || '';
+      const username = userObj.username || '';
+      const person = await db.person.create({
+        data: {
+          id: userObj.id,
+          firstName,
+          lastName,
+          username,
+          imageUrl: userObj.imageUrl,
+        },
+      });
+      people.push(person);
+    }
+    if (people.length == users.length) {
+      console.log('Successfully seeded users');
+    } else {
+      throw new Error();
+    }
+    return true;
+  } catch (error) {
+    console.error('Error seeding users:', error);
+    throw error;
+  }
+}
+
+seedUsers();
