049ae5add43ef553afdb6c48dcb30c3acdf43f8c
TheiaSurette <tsurette111@gmail.com>
2025-05-25 13:23:35 -0400

    test: verify pre-commit hooks work

---
+++ test-commit-hooks.ts
@@ -0,0 +1,6 @@
+// This is a test file to verify prettier and linting on commit
+const testFunction = () => {
+  console.log('This should be formatted by prettier');
+};
+
+export default testFunction;
